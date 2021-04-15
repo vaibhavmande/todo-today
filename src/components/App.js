7be15c9f2988ab27e024177397961f8377cd3375
Vaibhav Mande <vaibhavrmande@gmail.com>
2021-04-15 20:29:09 +0530

    moved circle to elements

--- src/components/App.js
+++ src/components/App.js
@@ -2,38 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import TasksContainer from './TasksContainer'
 import { FlexContainer } from './containers/FlexContainer'
+import { Circle } from './elements/Circle'
 
 const Container = styled(FlexContainer)`
   height: 100vh;
   background: linear-gradient(to right top, #ff9a9e, #fad0c4);
-`
-
-const Circle = styled.div`
-  height: 5rem;
-  width: 5rem;
-  position: absolute;
-  border-radius: 50%;
-  background: white;
-  background: linear-gradient(
-    to right bottom,
-    rgba(255, 255, 255, 0.8),
-    rgba(255, 255, 255, 0.3)
-  );
-  z-index: 1;
-
-  ${({ type }) =>
-    type == 'top'
-      ? `
-  top: 5%;
-  right: 15%;`
-      : `
-  bottom: 5%;
-  left: 10%;
-  `}
-
-  @media screen and (max-width: 325px) {
-    display: none;
-  }
 `
 
 export const App = () => (
